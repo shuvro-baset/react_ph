6b1874c955fa40a9abc905e5af4ec5274b20af71
shuvro baset <md.abdul.baset75@gmail.com>
2021-05-23 02:54:22 +0600

    data loading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,15 @@ function App() {
         {/* calling the component */}
          <Person></Person>
          <Person></Person>
+         {/* dynamic component */}
+         <DynamicPersonComponent name="Shuvro" job="Web Developer"></DynamicPersonComponent>
+         <DynamicPersonComponent name="Baset" job="React Developer"></DynamicPersonComponent>
+
+        {/* state changer  */}
+        <Counter></Counter>
+
+        {/* load user data */}
+        <Users></Users>
 
       </header>
     </div>
@@ -29,6 +39,55 @@ function Person(){
     <div style={personStyle}>
       <h1>Name: sakib al hasan</h1>
       <h3>favourite bd player</h3> 
+    </div>
+  )
+}
+// creating component dynamic way and passing it to component, in that case we have to pass props as parameters
+function DynamicPersonComponent(props) {
+  return (
+    <div style={{border: '1px solid yellow',margin: '10px',width: '400px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Profession: {props.job}</p>
+    </div>
+  )
+}
+
+// dynamic state changer
+function Counter() {
+  const [count, setCount] = useState(0);
+  // const handleIncrease = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+  // making easier function to increment and decrement
+  // const handleIncrease = () => setCount(count+1);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count+1)}>Increase</button>
+      <button onClick={() => setCount(count-1)}>decrease</button>
+
+    </div>
+  )
+}
+
+// data loading component
+function Users(){
+  // declare state. 
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+  return(
+    <div>
+      <h3>Dynamic Users: </h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li >)
+        }
+      </ul>
     </div>
   )
 }

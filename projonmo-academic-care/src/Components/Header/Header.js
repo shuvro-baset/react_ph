fe656c5fab3f6cdc1e3ef73907106207f2be67e7
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-27 22:55:29 +0600

    ||

--- projonmo-academic-care/src/Components/Header/Header.js
+++ projonmo-academic-care/src/Components/Header/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         // header div.
         <div className="container-fluid text-center p-3 bg-info">
-            <h1 className="">Academic Help Line </h1>
+            <h1 className="">Projonmo Academic Care</h1>
             <h4>Our Honorable Teachers</h4>
-            <h6> Our Monthly Budget for Teachers : <small className='fw-bolder'><span className='fw-bolder'>&#2547;</span></small> 1,00,000</h6>
+            <h6> Our Monthly Budget for Teachers : <small className='fw-bolder'><span className='fw-bolder'>&#2547;</span></small> 250000</h6>
         </div>
     );
 };

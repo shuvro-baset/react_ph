f4800e3ed63ef0ed459eab4a7fa711fea867d765
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-29 23:01:35 +0600

    error debugging wip

--- simple-route/src/App.js
+++ simple-route/src/App.js
@@ -1,16 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
-import Friends from './Components/Friends/Friends';
+import Friends from './Components/Friends';
+import FriendDetail from './Components/FriendDetail/FriendDetail';
+import PostDetail from './Components/Post/PostDetail';
+import About from './Components/About/About';
+import Culture from './Components/Culture/Culture';
+import NotFound from './Components/NotFound/NotFound';
+
 function App() {
   return (
     <div className="App">
-      <Router> 
+      <Router>
+        <Header></Header>
         <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
           <Route path="/home">
             <Home></Home>
+          </Route>
+          <Route path="/friends">
             <Friends></Friends>
+          </Route>
+          <Route path="/friend/:friendId">
+            <FriendDetail></FriendDetail>
+          </Route>
+          <Route path="/post/:postId">
+            <PostDetail></PostDetail>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+          <Route exact path="/about/culture">
+            <Culture></Culture>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
           </Route>
         </Switch>
       </Router>

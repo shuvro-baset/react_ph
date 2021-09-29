85af9072655a0ec4979c95c1b049c71828f42b37
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-29 17:51:33 +0600

    wip

--- simple-route/src/App.js
+++ simple-route/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Friends from './Components/Friends/Friends';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch></Switch>
+      <Router> 
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+            <Friends></Friends>
+          </Route>
+        </Switch>
       </Router>
     </div>
   );

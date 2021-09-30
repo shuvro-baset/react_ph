39ba9f00612d7bf17ebba7304970e868eeae1cd8
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-30 19:53:51 +0600

    header part done

--- my-resturant/src/App.js
+++ my-resturant/src/App.js
@@ -1,20 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { AppBar, Typography, Toolbar, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import Header from './Components/Header/Header';
+
 function App() {
   return (
     <div className="App">
-      <AppBar position="static">
-  <Toolbar variant="dense">
-    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" color="inherit" component="div">
-      Photos
-    </Typography>
-  </Toolbar>
-</AppBar>
+      <Header></Header>
     </div>
   );
 }
---
+++ my-resturant/src/Components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AppBar, Typography, Toolbar, IconButton, Button, TextField } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+const Header = () => {
+    return (
+        <div>
+            <AppBar position="static" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center'}} >
+                <Toolbar variant="dense"  >
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                        <HomeIcon color="secondary" fontSize="large" />
+                    </IconButton>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">About</Button>
+                    <Button color="inherit">Restaurant </Button>
+                    <Button color="inherit">Food</Button>
+                    <Button color="inherit">Meal</Button>
+                </Toolbar>
+            </AppBar>
+            <Typography><h1>Welcome to My Restaurant</h1></Typography>
+            <TextField id="outlined-basic" label="search food" variant="outlined" sx={{width: 500}} />
+        </div>
+    );
+};
+
+export default Header;

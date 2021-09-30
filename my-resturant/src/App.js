6e6f5bddea2773ac63ca5f127fbf0d218d298814
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-30 17:08:56 +0600

    wip

--- my-resturant/src/App.js
+++ my-resturant/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { AppBar, Typography, Toolbar, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+  <Toolbar variant="dense">
+    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" color="inherit" component="div">
+      Photos
+    </Typography>
+  </Toolbar>
+</AppBar>
     </div>
   );
 }

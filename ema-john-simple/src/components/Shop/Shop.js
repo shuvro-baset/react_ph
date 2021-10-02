a1f8154dd51f8fe5ff99230f148a5d2f14773a5c
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-02 15:55:11 +0600

    product

--- ema-john-simple/src/components/Shop/Shop.js
+++ ema-john-simple/src/components/Shop/Shop.js
@@ -7,31 +7,39 @@ import useCart from '../../hooks/useCart';
 import { Link } from 'react-router-dom';
 
 const Shop = () => {
+    // First time products load from fakedb
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useCart(products);
     // products to be rendered on the UI
     const [displayProducts, setDisplayProducts] = useState([]);
 
+    // First Effect for uploading products
     useEffect(() => {
         fetch('./products.JSON')
             .then(res => res.json())
             .then(data => {
-                setProducts(data);
+                setProducts(data); // update products state.
                 setDisplayProducts(data);
             });
     }, []);
 
 
-
+    // handle add to cart function. When a product is added to the cart.
     const handleAddToCart = (product) => {
+        // find the product that was selected is exist or not into the cart.
         const exists = cart.find(pd => pd.key === product.key);
         let newCart = [];
         if (exists) {
+            // filtering others products that was already in the cart.
             const rest = cart.filter(pd => pd.key !== product.key);
+            // set selected products quantity to property and Increase it.
             exists.quantity = exists.quantity + 1;
+            // set newCart
             newCart = [...rest, product];
         }
+        // if new product is selected for the first time.
         else {
+            // set quantity property for that product and set the value 1.
             product.quantity = 1;
             newCart = [...cart, product];
         }

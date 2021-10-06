7dfd749a9bb91a1bb1f1357eeec12a2240b0c904
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-06 06:53:22 +0600

    sigin done

--- simple-firebase-authentication/src/App.js
+++ simple-firebase-authentication/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from './firebase/firebase.initialize';
+const provider = new GoogleAuthProvider();
+initializeAuthentication()
 function App() {
+
+  const handleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+  }
   return (
     <div className="App">
-      
+      <button onClick={handleSignIn}>google-sign-in</button>
     </div>
   );
 }

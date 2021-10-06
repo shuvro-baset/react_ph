37017aed22c483a1a7d0cf43838da1b86657ac39
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-06 06:34:48 +0600

    initailize firebase authentication

---
+++ simple-firebase-authentication/src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCKJ6NxdNz9E2zjCd76tXh-c3Pdy6S7w18",
+    authDomain: "simple-firebase-authenti-fad01.firebaseapp.com",
+    projectId: "simple-firebase-authenti-fad01",
+    storageBucket: "simple-firebase-authenti-fad01.appspot.com",
+    messagingSenderId: "438432749071",
+    appId: "1:438432749071:web:8d88136e0f824b8821b57a"
+  };
+
+export default firebaseConfig;
